6b324367d1147657a3183c835201bfdadac32654
Anoosha Naz <163389690+anooshanaz@users.noreply.github.com>
2025-02-24 14:53:28 +0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+#Day 1 python challenge:
+
+#Print your first message 
+print('Hello Python Learner! 🛩️  Today is the first step in our coding journey.')
+
+#message store in variable
+name="Anoosha Naz"
+print(f'Hello {name}! 🛩️  Today is the first step in our coding journey.')
